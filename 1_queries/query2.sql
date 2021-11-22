cba594d2b94da7b2806370860ca19499e0828fac
Charlesdigital <charles.easton123@gmail.com>
2021-11-22 18:35:04 -0500

    add queries 1

---
+++ 1_queries/query2.sql
@@ -0,0 +1,10 @@
+--Select the total number of students who were in the first 3 cohorts.
+SELECT count(id)
+FROM students
+WHERE cohort_id IN (1,2,3);
+
+
+--  count
+-- -------
+--     48
+-- (1 row)
